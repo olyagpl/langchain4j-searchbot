8c160166ecf253df683c53483aeb30eee753a370
Olya Gupalo <olga.gupalo@oracle.com>
2024-06-14 15:33:18 +0200

    Enable Build Report

--- scripts/build-size-optimized-image.sh
+++ scripts/build-size-optimized-image.sh
@@ -3,6 +3,7 @@ native-image \
             -Os \
             -jar ./target/searchbot-1.0-jar-with-dependencies.jar \
             -H:+AllowDeprecatedBuilderClassesOnImageClasspath \
+            -H:+UnlockExperimentalVMOptions -H:+BuildReport \
             -o ./target/searchbot-optimized
 
 echo "What is the weather today?"
